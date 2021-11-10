a7bd2b72c9700d26315f38f83b0c530a0b1e7015
Vinícius Couto Tasso <vcoutasso@gmail.com>
2021-11-10 13:08:00 -0300

    Select multiple photos with PHPickerViewController

--- App/Spider-Verse/Sources/Scenes/Review/Presenter/Representable/PhotoPickerRepresentable.swift
+++ App/Spider-Verse/Sources/Scenes/Review/Presenter/Representable/PhotoPickerRepresentable.swift
@@ -5,15 +5,33 @@
 //  Created by Vinícius Couto on 10/11/21.
 //
 
+import PhotosUI
 import SwiftUI
+import UIKit
 
-struct PhotoPickerRepresentable: UIViewControllerRepresentable {
+protocol MultiplePhotoPicker {
+    var selectedImages: [UIImage] { get }
+    var isPresented: Bool { get }
+
+    func clearSelection()
+    func selectPhoto(_ image: UIImage)
+    func dismiss()
+}
+
+struct PhotoPickerRepresentable: MultiplePhotoPicker, UIViewControllerRepresentable {
+    // MARK: - Dependencies
+
+    let pickerConfiguration: PHPickerConfiguration
+
     // MARK: - Attributes
 
-    @Binding var selectedImage: UIImage
+    @Binding var selectedImages: [UIImage]
+    @Binding var isPresented: Bool
 
-    func makeUIViewController(context: Context) -> UIImagePickerController {
-        let picker = UIImagePickerController()
+    // MARK: - Representable methods
+
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        let picker = PHPickerViewController(configuration: pickerConfiguration)
         picker.delegate = context.coordinator
 
         return picker
@@ -23,14 +41,28 @@ struct PhotoPickerRepresentable: UIViewControllerRepresentable {
         Coordinator(photoPicker: self)
     }
 
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+
+    // MARK: - Picker methods
+
+    func clearSelection() {
+        selectedImages.removeAll()
+    }
+
+    func selectPhoto(_ image: UIImage) {
+        selectedImages.append(image)
+    }
+
+    func dismiss() {
+        isPresented = false
+    }
 
     // MARK: - Inner types
 
     final class Coordinator: NSObject {
         // MARK: - Attributes
 
-        let photoPicker: PhotoPickerRepresentable
+        let photoPicker: MultiplePhotoPicker
 
         // MARK: - Object lifecycle
 
@@ -40,14 +72,34 @@ struct PhotoPickerRepresentable: UIViewControllerRepresentable {
     }
 }
 
-extension PhotoPickerRepresentable.Coordinator: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+extension PhotoPickerRepresentable.Coordinator: PHPickerViewControllerDelegate {
+    // MARK: - Type aliases
+
+    private typealias ResultType = UIImage
+
     // MARK: - Delegate methods
 
-    func imagePickerController(_ picker: UIImagePickerController,
-                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        guard let image = info[.originalImage] as? UIImage else { return }
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        photoPicker.clearSelection()
+        parseAndUpdatePickingResults(results)
+        photoPicker.dismiss()
+    }
 
-        photoPicker.selectedImage = image
-        picker.dismiss(animated: true)
+    // MARK: - Helper methods
+
+    private func parseAndUpdatePickingResults(_ results: [PHPickerResult]) {
+        results.filter { $0.itemProvider.canLoadObject(ofClass: ResultType.self) }
+            .forEach {
+                $0.itemProvider.loadObject(ofClass: ResultType.self,
+                                           completionHandler: loadImageCompletionHandler(image:error:))
+            }
+    }
+
+    private func loadImageCompletionHandler(image: NSItemProviderReading?, error: Error?) {
+        if let image = image as? UIImage {
+            photoPicker.selectPhoto(image)
+        } else if let error = error {
+            print("Could not load image. Got error: '\(error.localizedDescription)'")
+        }
     }
 }
--- App/Spider-Verse/Sources/Scenes/Review/Presenter/View/TestingPhotoPickerView.swift
+++ App/Spider-Verse/Sources/Scenes/Review/Presenter/View/TestingPhotoPickerView.swift
@@ -5,15 +5,22 @@
 //  Created by Vinícius Couto on 10/11/21.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct TestingPhotoPickerView: View {
+    // MARK: - Attributes
+
     @State private var isShowingPhotoPicker = false
-    @State private var image = UIImage(systemName: "person.fill")!
+    @State private var images = [imagePlaceholder]
+
+    private static let imagePlaceholder = UIImage(systemName: "person.fill")!
+
+    // MARK: - View
 
     var body: some View {
         VStack {
-            Image(uiImage: image)
+            Image(uiImage: images.first ?? TestingPhotoPickerView.imagePlaceholder)
                 .resizable()
                 .scaledToFill()
                 .frame(width: 100, height: 100, alignment: .center)
@@ -24,7 +31,20 @@ struct TestingPhotoPickerView: View {
             Spacer()
         }
         .sheet(isPresented: $isShowingPhotoPicker) {
-            PhotoPickerRepresentable(selectedImage: $image)
+            PhotoPickerRepresentable(pickerConfiguration: PickerConfigurationFactory.make(),
+                                     selectedImages: $images,
+                                     isPresented: $isShowingPhotoPicker)
+        }
+    }
+}
+
+extension TestingPhotoPickerView {
+    enum PickerConfigurationFactory {
+        static func make() -> PHPickerConfiguration {
+            var configuration = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+            configuration.filter = .images
+            configuration.selectionLimit = 0
+            return configuration
         }
     }
 }
