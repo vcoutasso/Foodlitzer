17559cd58973225066df76bd70ce07a96a1881d7
Alessandra Souza da Silva <alessandrass11@yahoo.com>
2021-11-12 12:58:00 -0300

    GooglePlaces

--- App/SpiderVerse/Sources/Application/Application.swift
+++ App/SpiderVerse/Sources/Application/Application.swift
@@ -1,8 +1,20 @@
+import GooglePlaces
 import SwiftUI
 
 @main
 struct Application: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    init() {
+        let path = Bundle.main.path(forResource: "GooglePlaces-info", ofType: "plist")
+        print(path)
+        let xml = FileManager.default.contents(atPath: path!)
+        print(xml)
+        let api_key = try? PropertyListSerialization.propertyList(from: xml!,
+                                                                  options: .mutableContainersAndLeaves,
+                                                                  format: nil) as? [String]
+        print(api_key)
+        GMSPlacesClient.provideAPIKey(api_key!.first!)
+    }
 
     var body: some Scene {
         WindowGroup {
