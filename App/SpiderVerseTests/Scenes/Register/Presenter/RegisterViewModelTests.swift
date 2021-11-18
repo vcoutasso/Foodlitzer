aee17c0ae63e58a2c59a96a84cb8ccef266805a5
Vinícius Couto Tasso <vcoutasso@gmail.com>
2021-11-18 10:07:50 -0300

    Valid email and password with firebase

--- App/SpiderVerseTests/Scenes/Register/Presenter/RegisterViewModelTests.swift
+++ App/SpiderVerseTests/Scenes/Register/Presenter/RegisterViewModelTests.swift
@@ -4,13 +4,8 @@ import XCTest
 final class RegisterViewModelTests: XCTestCase {
     // MARK: - System under test
 
-    // FIXME: Mock dependencies and test them separately
-    private let emailValidator = ValidateEmailUseCase()
-    private let passwordValidator = ValidatePasswordUseCase()
     private let authenticationServiceDummy = AuthenticationServiceDummy()
-    private lazy var sut = RegisterViewModel(emailValidationService: emailValidator,
-                                             passwordValidationService: passwordValidator,
-                                             authenticationService: authenticationServiceDummy)
+    private lazy var sut = RegisterViewModel(authenticationService: authenticationServiceDummy)
 
     // MARK: - Test doubles
 
@@ -35,72 +30,4 @@ final class RegisterViewModelTests: XCTestCase {
     }
 
     // MARK: - Unit tests
-
-    func testIsValidEmailShouldAllowValidAddresses() {
-        // Given
-        let validList = Fixtures.EmailAddresses.validEmails
-        let resultStub = true
-        let expectedResult = [Bool](repeating: resultStub, count: validList.count)
-        var actualResult = [Bool]()
-
-        // When
-        validList.forEach {
-            let isValid = sut.isValid(email: $0)
-            actualResult.append(isValid)
-        }
-
-        // Then
-        XCTAssertEqual(expectedResult, actualResult)
-    }
-
-    func testIsValidEmailShouldForbidInvalidAddresses() {
-        // Given
-        let invalidList = Fixtures.EmailAddresses.invalidEmails
-        let resultStub = false
-        let expectedResult = [Bool](repeating: resultStub, count: invalidList.count)
-        var actualResult = [Bool]()
-
-        // When
-        invalidList.forEach {
-            let isValid = sut.isValid(email: $0)
-            actualResult.append(isValid)
-        }
-
-        // Then
-        XCTAssertEqual(expectedResult, actualResult)
-    }
-
-    func testIsValidPasswordShouldAllowValidPasswords() {
-        // Given
-        let validList = Fixtures.Passwords.validPasswords
-        let resultStub = true
-        let expectedResult = [Bool](repeating: resultStub, count: validList.count)
-        var actualResult = [Bool]()
-
-        // When
-        validList.forEach {
-            let isValid = sut.isValid(password: $0)
-            actualResult.append(isValid)
-        }
-
-        // Then
-        XCTAssertEqual(expectedResult, actualResult)
-    }
-
-    func testIsValidPasswordShouldForbidInvalidPasswords() {
-        // Given
-        let invalidList = Fixtures.Passwords.invalidPasswords
-        let resultStub = false
-        let expectedResult = [Bool](repeating: resultStub, count: invalidList.count)
-        var actualResult = [Bool]()
-
-        // When
-        invalidList.forEach {
-            let isValid = sut.isValid(password: $0)
-            actualResult.append(isValid)
-        }
-
-        // Then
-        XCTAssertEqual(expectedResult, actualResult)
-    }
 }
