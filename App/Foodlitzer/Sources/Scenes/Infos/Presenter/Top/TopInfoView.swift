fcfa274e5ee0628ae528c7714c609da8fb159fed
Eros Maurilio <eros.maurilio@gmail.com>
2021-11-29 08:45:01 -0300

    Fix infopageview layout

--- App/Foodlitzer/Sources/Scenes/Infos/Presenter/Top/TopInfoView.swift
+++ App/Foodlitzer/Sources/Scenes/Infos/Presenter/Top/TopInfoView.swift
@@ -15,42 +15,28 @@ struct TopInfoView: View {
     var body: some View {
         VStack(spacing: 0) {
             HStack {
-                Button {
-                    showAlert = true
-                } label: {
-                    Label("", systemImage: "chevron.backward")
-                        .modifier(SimbolosPadrao())
-                }
                 Spacer()
                 Text(restaurantName)
                     .frame(height: 33)
                     .padding(.horizontal, 10)
-                    .modifier(FontePadrao())
+                    .font(.sfCompactText(.light, size: 14))
+
                 Spacer()
-                Button {
-                    showAlert = true
-                } label: {
-                    Label("", systemImage: "info.circle")
-                        .modifier(SimbolosPadrao())
-                }
             }
+            .background(Color.white.border(Color.black, width: 0.3))
+            .frame(height: 45)
             .padding(.horizontal, 30)
             .padding(.vertical, 10)
 
-            HStack {
-                Image(systemName: "speaker.wave.3")
-                    .modifier(SimbolosPadrao())
-                Spacer()
-            }
-            .padding(.horizontal, 30)
-            .padding(.vertical, 10)
-            .alert(isPresented: $showAlert) {
-                Alert(title: Text("Info"),
-                      message: Text("As informações básicas sobre este restaurante são fornecidas pelo Google."),
-                      dismissButton: .default(Text("OK")))
-            }
             Spacer()
+//            HStack {
+//                Image(systemName: "speaker.wave.3")
+//                    .modifier(SimbolosPadrao())
+//                Spacer()
+//            }
         }
         .padding(.top, 10)
+
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
